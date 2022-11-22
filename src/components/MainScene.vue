830c43c72e969bd5322e8fe6c827c7dc305af3c5
kuiyuan.qiu <kuiyuan.qiu@southgis.com>
2022-11-22 23:39:21 +0800

    初始化工程

---
+++ src/components/MainScene.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import * as Cesium from 'cesium';
+import { onMounted, ref } from 'vue';
+
+onMounted(() => {
+  let ele = ref("cesiumContainer")
+  window.cesium = new Cesium.Viewer(ele.value)
+})
+</script>
+
+<template>
+  <div id="cesiumContainer" ref="cesiumContainer"></div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
+import externalGlobals from 'rollup-plugin-external-globals'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'cesium': path.resolve(__dirname, "./public/lib/Cesium.js")
+    }
+  },
+  server: {
+    fs: {
+      strict:false //lmw add 4 消除html文件中引入cesium的警告
+    }
+  },
+  optimizeDeps: {
+    include: ['cesium']
+  },
+  build: {
+    rollupOptions: {
+      external: ['cesium'],
+      plugins: [
+        externalGlobals({
+          "cesium": "Cesium"
+        })
+      ]
+    }
+  }
+})
